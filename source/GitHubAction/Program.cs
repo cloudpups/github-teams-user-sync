582ad1adf80525c2c8cb1bd8a6e49728027d2e26
Joshua Miller <joshdan65@gmail.com>
2022-05-29 16:36:19 -0500

    Eliminate duplicate outputs, change wording
    Wording change was done in case this tool was used not as an Action, but as a standalone tool.

--- source/GitHubAction/Program.cs
+++ source/GitHubAction/Program.cs
@@ -79,99 +79,104 @@ await host.RunAsync();
 
 static async Task StartTeamSyncAsync(RenderedInput inputs, IHost host)
 {
-	if(!inputs.GitHubTeamNames.Any())
+    if (!inputs.GitHubTeamNames.Any())
     {
-		Console.WriteLine("No teams found to syncronize!");
-		Environment.Exit(0);
-	}
+        Console.WriteLine("No teams found to syncronize!");
+        Environment.Exit(0);
+    }
 
-	// Azure AD Group and GitHub Team Name must match (my opinion, baked into this tool)	
-	var groupDisplayNames = inputs.GitHubTeamNames.Concat(new[] { inputs.OrganizationMembersGroup }).Distinct().ToDictionary(t => t);
+    // Azure AD Group and GitHub Team Name must match (my opinion, baked into this tool)	
+    var groupDisplayNames = inputs.GitHubTeamNames.Concat(new[] { inputs.OrganizationMembersGroup }).Distinct().ToDictionary(t => t);
 
-	var tenantId = inputs.TenantId;
-	var clientId = inputs.ClientId;
-	var clientSecret = inputs.ClientSecret;
+    var tenantId = inputs.TenantId;
+    var clientId = inputs.ClientId;
+    var clientSecret = inputs.ClientSecret;
 
-	var tokenAuth = new Credentials(inputs.OrgAdministerToken);
-	
-	var org = inputs.GitHubRepositoryOwner;
+    var tokenAuth = new Credentials(inputs.OrgAdministerToken);
 
-	var emailPrepend = inputs.EmailPrepend;
-	var itemsToReplace = inputs.EmailTextToReplace;
-	var emailAppend = inputs.EmailAppend;
+    var org = inputs.GitHubRepositoryOwner;
 
-	// The client credentials flow requires that you request the
-	// /.default scope, and preconfigure your permissions on the
-	// app registration in Azure. An administrator must grant consent
-	// to those permissions beforehand.
-	var scopes = new[] { "https://graph.microsoft.com/.default" };
-	// using Azure.Identity;
-	var options = new TokenCredentialOptions
-	{
-		AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
-	};
-	// https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
-	var clientSecretCredential = new ClientSecretCredential(
-		tenantId, clientId, clientSecret, options);
-	var graphClient = new GraphServiceClient(clientSecretCredential);
-	var activeDirectoryFacade = new ActiveDirectoryFacade(graphClient);
+    var emailPrepend = inputs.EmailPrepend;
+    var itemsToReplace = inputs.EmailTextToReplace;
+    var emailAppend = inputs.EmailAppend;
+
+    // The client credentials flow requires that you request the
+    // /.default scope, and preconfigure your permissions on the
+    // app registration in Azure. An administrator must grant consent
+    // to those permissions beforehand.
+    var scopes = new[] { "https://graph.microsoft.com/.default" };
+    // using Azure.Identity;
+    var options = new TokenCredentialOptions
+    {
+        AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+    };
+    // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+    var clientSecretCredential = new ClientSecretCredential(
+        tenantId, clientId, clientSecret, options);
+    var graphClient = new GraphServiceClient(clientSecretCredential);
+    var activeDirectoryFacade = new ActiveDirectoryFacade(graphClient);
 
     var client = new GitHubClient(new ProductHeaderValue("groups-to-teams-sync"))
     {
         Credentials = tokenAuth
     };
 
-	var gitHubFacade = new GitHubFacadeCacheDecorator(new GitHubFacade(client), new MemoryCache(new MemoryCacheOptions()));
+    var gitHubFacade = new GitHubFacadeCacheDecorator(new GitHubFacade(client), new MemoryCache(new MemoryCacheOptions()));
 
-	var emailToCloudIdBuilder = EmailToCloudIdBuilder.Build(emailPrepend, emailAppend, itemsToReplace);
+    var emailToCloudIdBuilder = EmailToCloudIdBuilder.Build(emailPrepend, emailAppend, itemsToReplace);
 
-	var groupSyncer = GroupSyncerBuilder.Build(activeDirectoryFacade, gitHubFacade, emailToCloudIdBuilder);
+    var groupSyncer = GroupSyncerBuilder.Build(activeDirectoryFacade, gitHubFacade, emailToCloudIdBuilder);
 
-	var groupsToSyncronize = groupDisplayNames.Select(g => new
+    var groupsToSyncronize = groupDisplayNames.Select(g => new
     {
-		Key = g.Key,
-		Value = new TeamDefinition("ActiveDirectory", g.Key)
-	}).ToDictionary(o => o.Key, o => o.Value);
+        Key = g.Key,
+        Value = new TeamDefinition("ActiveDirectory", g.Key)
+    }).ToDictionary(o => o.Key, o => o.Value);
 
-	Console.WriteLine("This Action will attempt to syncronize the following groups:");
-	foreach (var group in groupsToSyncronize)
+    Console.WriteLine("This Action will attempt to syncronize the following groups:");
+    foreach (var group in groupsToSyncronize)
     {
-		Console.WriteLine($"* {group.Key}");
-	}
+        Console.WriteLine($"* {group.Key}");
+    }
 
-	var usersThatMayNotExist = new List<GitHubUser>();
+    var usersWithSyncIssues = new List<GitHubUser>();
 
-	if (!inputs.OrganizationMembersGroup.IsEmptyOrWhitespace())
+    if (!inputs.OrganizationMembersGroup.IsEmptyOrWhitespace())
     {
-		var memberSyncResult = await groupSyncer.SyncronizeMembersAsync(org, groupsToSyncronize[inputs.OrganizationMembersGroup]);
-		usersThatMayNotExist.AddRange(memberSyncResult.UsersWithSyncIssues);
-	}	
+        var memberSyncResult = await groupSyncer.SyncronizeMembersAsync(org, groupsToSyncronize[inputs.OrganizationMembersGroup]);
+        usersWithSyncIssues.AddRange(memberSyncResult.UsersWithSyncIssues);
+    }
 
-	var groupSyncResult = await groupSyncer.SyncronizeGroupsAsync(org, groupsToSyncronize.Values);
+    var groupSyncResult = await groupSyncer.SyncronizeGroupsAsync(org, groupsToSyncronize.Values);
 
-	usersThatMayNotExist.AddRange(groupSyncResult.UsersWithSyncIssues);
+    usersWithSyncIssues.AddRange(groupSyncResult.UsersWithSyncIssues);
 
-	if (usersThatMayNotExist.Any())
-    {
-		Console.WriteLine("################################################");
-		Console.WriteLine();
-		Console.WriteLine("This Action had issues with the following users:");
-		Console.WriteLine();
-		foreach (var user in usersThatMayNotExist)
-        {
-			Console.WriteLine($"{user.Email} ==> {user.GitHubId}");
-		}
-		Console.WriteLine();
-		Console.WriteLine("################################################");
-	}
+    WriteConsoleOutput(usersWithSyncIssues.DistinctBy(g => g.Email).ToHashSet());
 
-	var formattedUsersThatMayNotExist = JsonConvert.SerializeObject(usersThatMayNotExist) ?? "";
-
-	Console.WriteLine("Complete!");
-
-	Console.WriteLine($"::set-output name=users-with-sync-issues::{formattedUsersThatMayNotExist}");
-
-	await Task.CompletedTask;
+    await Task.CompletedTask;
 
     Environment.Exit(0);
+}
+
+static void WriteConsoleOutput(ISet<GitHubUser> usersWithSyncIssues)
+{
+    if (usersWithSyncIssues.Any())
+    {
+        Console.WriteLine("################################################");
+        Console.WriteLine();
+        Console.WriteLine("There were issues with the following users:");
+        Console.WriteLine();
+        foreach (var user in usersWithSyncIssues)
+        {
+            Console.WriteLine($"{user.Email} ==> {user.GitHubId}");
+        }
+        Console.WriteLine();
+        Console.WriteLine("################################################");
+    }
+
+    var formattedUsersWithSyncIssues = JsonConvert.SerializeObject(usersWithSyncIssues) ?? "";
+
+    Console.WriteLine("Complete!");
+
+    Console.WriteLine($"::set-output name=users-with-sync-issues::{formattedUsersWithSyncIssues}");
 }
