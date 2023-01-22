904e516ab3b0af58f0159c9e019a85ecf4363293
JoshuaTheMiller <joshuadanielmiller@outlook.com>
2023-01-22 10:34:57 -0600

    :bug: fix backwards compatability

--- source/GitHubAction/InputsFromFile.cs
+++ source/GitHubAction/InputsFromFile.cs
@@ -4,5 +4,11 @@
     {
         public string OrganizationMembersGroup { get; init; } = string.Empty;
         public IEnumerable<string> GitHubTeamNames { get; init; } = Enumerable.Empty<string>();
+        [Obsolete("DO NOT USE")]
+        public bool CreateDeployment { get; init; } = false;
+        [Obsolete("DO NOT USE")]
+        public string EmailAppend { get; init; } = string.Empty;
+        [Obsolete("DO NOT USE")]
+        public IEnumerable<string> EmailTextToReplaceRules { get; init; } = Enumerable.Empty<string>();
     }
 }
