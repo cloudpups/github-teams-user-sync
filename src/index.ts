9126e84bbae58fc1d65670610fe0eb96098435c3
Joshua Miller <joshdan65@gmail.com>
2023-03-21 15:38:14 -0500

    Use openapi to define api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-import express from "express"
+import express from "express";
+import {OpenAPIBackend, Document} from 'openapi-backend';
+import yaml from "js-yaml";
+import fs from "fs";
+import path from "node:path";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello world!");
-})
+// TODO: fix/determine why OpenAPIBackend is having issues loading files on its own...
+const doc = yaml.load(fs.readFileSync(path.resolve(__dirname, 'openapi.yaml'), 'utf8'));
+
+const castDoc = doc as Document;
+
+const api = new OpenAPIBackend({ definition: castDoc });
+
+api.register({
+    getPets: (c, req, res) => res.status(200).json({ result: 'what' }),
+    validationFail: (c, req, res) => res.status(400).json({ err: c.validation.errors }),
+    notFound: (c, req, res) => res.status(404).json({ err: 'not found' }),
+  });
+
+app.use(express.json());
+app.use((req:any, res:any) => api.handleRequest(req, req, res));
 
 app.listen(port);
