eef92d68b376767c728887471f63a1bf61a6a7ff
Joshua Miller <joshdan65@gmail.com>
2023-03-21 15:45:31 -0500

    Add stubs for additional endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,15 @@
 import { getCurrentRateLimitHandler } from "./handlers/getCurrentRateLimit";
 import { getInstalledOrgsHandler } from "./handlers/getInstalledOrgs";
 
+function notImplementedHandler(c: any, req: any, res: any) {
+    return res
+        .status(404)
+        .json({ status: 501, err: "No handler registered for operation" });
+}
+
 export const routes = {
     getInstalledOrgs: getInstalledOrgsHandler,
-    getCurrentRateLimit: getCurrentRateLimitHandler
+    getCurrentRateLimit: getCurrentRateLimitHandler,
+    syncOrg: notImplementedHandler,
+    syncAllOrgs: notImplementedHandler
 }
