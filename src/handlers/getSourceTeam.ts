031847d3a5f8e1920c50a183c8877192708ece6d
JoshuaTheMiller <joshua.d.miller@optum.com>
2023-08-07 14:01:33 -0500

    Add AI logger and more logs

--- src/handlers/getSourceTeam.ts
+++ src/handlers/getSourceTeam.ts
@@ -1,6 +1,7 @@
 import { Context } from "openapi-backend";
 import type { Request, Response } from "express";
 import { SearchAllAsync } from "../services/ldapClient";
+import { Log } from "../logging";
 
 export async function getSourceTeamHandler(
     c: Context,
@@ -9,7 +10,11 @@ export async function getSourceTeamHandler(
 ) {    
     const teamName = _req.query.teamName as string;
 
+    Log(`Searching SoT for ${teamName}`)
+
     const result = await SearchAllAsync(teamName)
+
+    Log(`Found in SoT:  ${JSON.stringify(result)}`)    
 
     return res.status(200).json(result);
 }
--- src/logging.ts
+++ src/logging.ts
@@ -1,13 +1,48 @@
+import appInsights from "applicationinsights";
+import TelemetryClient from "applicationinsights/out/Library/TelemetryClient";
 
-function log(s:any) {
-    console.log(s);
+export interface ILogger {
+    Log(s:any):void
+    LogError(s:any):void
 }
 
-function logError(s:any) {
-    console.log(s);
+class AiLogger implements ILogger {
+    client: TelemetryClient;
+    
+    constructor(client: TelemetryClient) {
+        this.client = client;
+    }
+    Log(s: any): void {
+        throw new Error("Method not implemented.");
+    }
+    LogError(s: any): void {
+        throw new Error("Method not implemented.");
+    }
 }
 
-export {
-    log as Log,
-    logError as LogError,
+class StdLogger implements ILogger {
+    Log(s: any): void {
+        console.log(s);
+    }
+    LogError(s: any): void {
+        console.log(s);
+    }
 }
+
+let logger: ILogger = new StdLogger();
+
+if(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
+    appInsights.setup().start();
+
+    const telemetryClient = appInsights.defaultClient;
+
+    logger = new AiLogger(telemetryClient);
+}
+
+export function Log(s:any) {
+    logger.Log(s);
+} 
+
+export function LogError(s:any) {
+    logger.LogError(s);
+} 
